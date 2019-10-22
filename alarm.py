92d02e046b2e933d5318306797e38898f7118138
bolt25 <dharmik255@gmail.com>
2019-10-22 22:57:56 +0530

    alarm goes off in....

--- alarm.py
+++ alarm.py
@@ -1,21 +1,19 @@
 from datetime import datetime
 def alarm(time):
 
-    time = time.split(':')
+    time = time.split(':') #splits hours and minutes
     
-    wake_up_time = map(int, time)
+    wake_up_time = map(int, time) #converts str to int
     
-    wake_up_time =list(wake_up_time)
+    wake_up_time =list(wake_up_time) #converted into list
     
-    wake_up_hour = wake_up_time[0]
+    wake_up_hour = wake_up_time[0] 
     
     wake_up_minute = wake_up_time[1]
-    
-    wake_up_second = 0
 
-    now = datetime.now().time()
+    now = datetime.now().time()  #gets current time
     
-    current_hour, current_minute, current_second =now.hour, now.minute, now.second
+    current_hour, current_minute =now.hour, now.minute
 
     hours_remaining = (wake_up_hour-current_hour) % 24,
     hours_remaining = hours_remaining[0]
