fc8e8bcb42413f000966e75f830749cf94137ab5
Chester97 <kkowalczuk27@gmail.com>
2019-10-22 21:12:46 +0200

    changing names, delete days from formatFunc

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-const convertTime = (motiveTime, clockStop, convertToMs, convertToHours) => {
+const outputFormatter = (motiveTime, clockStop, convertToMs, convertToHours) => {
     const convertedMotive = motiveTime.split(":");
     const convertedClockStop = clockStop.split(":");
     const msMotive = convertToMs(convertedMotive[0],convertedMotive[1]);
     const msClockStop = convertToMs(convertedClockStop[0],convertedClockStop[1]);
-    const maxNum = Math.max(...[msMotive,msClockStop]);
-    const minNum = Math.min(...[msClockStop,msMotive]);
+    const maxNum = Math.max(msMotive,msClockStop);
+    const minNum = Math.min(msClockStop,msMotive);
     if(maxNum && minNum > 0) {
         const getFullTimeOfSleep = (maxNum - minNum);
         return convertToHours(getFullTimeOfSleep);
+    }else {
+        return null;
     }
 }
-const getMiliseconds = (h,m) => {
+const getMiliseconds = (h=0,m=0) => {
     return ((h*60*60+m*60)*1000);
 }
 
-function timeConversion(millisec) {
+function formatTimeToString(millisec) {
 
     const seconds = (millisec / 1000).toFixed(1);
     const minutes = (millisec / (1000 * 60)).toFixed(1);
     const hours = (millisec / (1000 * 60 * 60)).toFixed(1);
-    const days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
 
     if (seconds < 60) {
         return seconds + " Sec";
     } else if (minutes < 60) {
         return minutes + " Min";
-    } else if (hours < 24) {
-        return hours + " Hrs";
     } else {
-        return days + " Days"
-    }
+        return hours + " Hrs";
+    } 
 }
-console.log(convertTime("5:15", "21:10", getMiliseconds,timeConversion));
+console.log(outputFormatter("5:15", "21:10", getMiliseconds,formatTimeToString));
